2da7de90ca48ba60558c3980ece48aae954c18b1
Derrick Abankwa <dkabankwa@yahoo.com>
2022-11-27 20:23:53 -0800

    fix

--- tests/test_app.py
+++ tests/test_app.py
@@ -14,7 +14,7 @@ def test_do_two():
     assert app.do_two() == 2
 
 def test_do_three():
-    assert app.do_three == 3
+    assert app.do_three() == 3
 
 def test_do_four():
-    assert app.do_four == 4
+    assert app.do_four() == 4
